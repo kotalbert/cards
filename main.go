5ab2576c2a4ec7c9e653c3d2a5b611220ca9097a
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2023-12-29 21:49:36 +0100

    Add cards slice

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 
-	card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
 
-	fmt.Println(card)
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 
 }
