855a8b07cfc9d73baf6b037776fa8e3eb3316b9a
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-01-04 12:33:26 +0100

    Implement tests for new deck

--- deck_test.go
+++ deck_test.go
@@ -2,25 +2,37 @@ package main
 
 import "testing"
 
+var d = newDeck()
+
 // Make sure deck is created with correct number of cards (52)
 func TestNewDeckSize(t *testing.T) {
-	d := newDeck()
 
 	e := 52
 	a := len(d)
 
-	if a != e {
+	if e != a {
 		t.Errorf("Expected deck length of %d, but got %d", e, a)
 	}
 }
 
 // Make sure first card in new deck is "Ace of Spades"
 func TestNewDeckFirstCard(t *testing.T) {
-	t.Fatalf("Implement")
+
+	e := "Ace of Spades"
+	a := d[0]
+
+	if e != a {
+		t.Errorf("Expected %s, but got %s", e, a)
+	}
 
 }
 
 // Make sure first card in new deck is "King of Clubs"
 func TestNewDeckLastCard(t *testing.T) {
-	t.Fatalf("Implement")
+	e := "King of Clubs"
+	a := d[len(d)-1]
+
+	if e != a {
+		t.Errorf("Expected %s, but got %s", e, a)
+	}
 }
